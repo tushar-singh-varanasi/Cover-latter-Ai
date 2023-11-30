570145b9edb3312683d257908bb83e567ec61b9f
Tushar Singh <tusharthakur0089@gmail.com>
2023-11-30 13:11:28 +0530

    final

--- check.py
+++
@@ -1,9 +0,0 @@
- 
-from bardapi import Bard
-
-import os
-os.environ['_BARD_API_KEY']="dQhnUsOnWpl6lL6q5-FOw1rf_-hddblWryrc7LMBYFF3XNKFySzQiMI8Wes-Ck2X2sg9wg."
-h1=input("enter somethings ;")
-
-print(Bard().get_answer(h1)['content'])
-
